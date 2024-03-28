86b2cd40159f09dcadc5d55ac82271e8bcf0de03
louisdesc <louisdesclous@gmail.com>
2024-03-28 15:29:41 +0100

    Handle multiple jobs_info file + improve fiability (gpt export json)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os,json
 from openai import OpenAI
 
 def read_file(file_path):
@@ -6,12 +6,14 @@ def read_file(file_path):
         return f.readlines()
 
 def create_openai_completion(model, messages):
+    from openai import OpenAI
     client = OpenAI()
     try:
         completion = client.chat.completions.create(
             model=model,
             messages=messages,
-            temperature= 0.15
+            temperature= 0.15,
+            response_format={ "type": "json_object" }
         )
         return completion
     except Exception as e:
@@ -19,31 +21,16 @@ def create_openai_completion(model, messages):
         return None
 
 
-
 def prepare_messages(profile_info, job_info, letter_format):
     return [
-        {"role": "system", "content": "Je suis un assistant qui permet de remplir les lettres de motivation pour vos candidatures. Je fais le lien entre vos expériences, vos compétences et l'offre d'emploi."},
+        {"role": "system", "content": "Je suis un assistant qui permet de remplir les lettres de motivation pour vos candidatures. Je fais le lien entre vos expériences, vos compétences et l'offre d'emploi. Le schema JSON doit contenir : Entreprise, Poste, Objet, Contenu"},
         {"role": "user", "content": f"Voici mes compétences et expériences : {profile_info}"},
         {"role": "system", "content": "Donnez moi l'offre d'emploi."},
         {"role": "user", "content": f"Voici le contenu de l'offre d'emploi : {job_info}"},
         {"role": "system", "content": "Donne moi le format type de lettre de motivation que je dois suivre."},
-        {"role": "user", "content": f"Voici le format type que j'aimerais que tu suives : {letter_format}"},
+        {"role": "user", "content": f"Voici le format type de contenu que j'aimerais que tu suives : {letter_format}"},
     ]
 
-def extract_text_between_markers(text, start_marker, end_marker):
-    try:
-        start_index = text.find(start_marker)
-        if start_index == -1:
-            raise ValueError(f"Start marker '{start_marker}' not found in text.")
-        start_index += len(start_marker)
-        end_index = text.find(end_marker, start_index)
-        if end_index == -1:
-            return text[start_index:].strip()
-        return text[start_index:end_index].strip()
-    except Exception as e:
-        print(f"An error occurred while extracting text: {e}")
-        return None
-    
 
 
 def process_job_application(profile_info, letter_raw, job_info_filename, jobs_directory):
@@ -56,20 +43,17 @@ def process_job_application(profile_info, letter_raw, job_info_filename, jobs_di
     
 
     if completion:
-        result_raw = completion.choices[0].message.content
-        poste_entreprise = extract_text_between_markers(result_raw, "Poste & Entreprise : ", "\nObjet")
-        letter = extract_text_between_markers(result_raw, "Objet : ", "DESCLOUS") + " DESCLOUS"
-        print(poste_entreprise)
-        print(letter)
+        result_raw = json.loads(completion.choices[0].message.content)
 
         # Prepare export content
         link_to_the_job = job_info[0]
-        export_content = f"{link_to_the_job}\n\n{letter}"
+        export_content = f"{link_to_the_job}\n\n{result_raw['Objet']}\n{result_raw['Contenu']}"
         
         # Define export path
         export_directory = os.path.join(jobs_directory, "..", "CL")
         os.makedirs(export_directory, exist_ok=True)
-        file_path = os.path.join(export_directory, f"CL - {poste_entreprise.replace('/', '-')}.txt")
+        file_path = os.path.join(export_directory, f"CL/{result_raw['Entreprise']} - {result_raw['Poste'].replace('/', '-')}.txt")
+
         
         # Write the content to file
         with open(file_path, "w", encoding='utf-8') as file:
